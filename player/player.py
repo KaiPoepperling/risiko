df0cb14a669380a25ee1e20b2e0222abfea62514
Kai Pöpperling <kai.poepperling@5minds.de>
2016-06-02 11:00:09 +0200

    Add player class

---
+++ player/player.py
@@ -0,0 +1,9 @@
+class Player(object):
+    """
+    """
+    def __init__(self, name, color):
+        """
+        """
+        self._name = name
+        self._color = color
+        self._hand = None
