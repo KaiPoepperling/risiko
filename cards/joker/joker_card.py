e39a523c760fdb6b31d46c87671887ffaacc2434
Kai Pöpperling <kai.poepperling@5minds.de>
2016-06-02 10:58:35 +0200

    Add the joker card

---
+++ cards/joker/joker_card.py
@@ -0,0 +1,27 @@
+class JokerCard(object):
+    """
+    """
+    def __init__(self):
+        """
+        """
+        self._name = 'joker'
+        self._continent = None
+        self._unit = 'joker'
+
+    @property
+    def name(self):
+        """
+        """
+        return self._name
+
+    @property
+    def continent(self):
+        """
+        """
+        return self._continent
+
+    @property
+    def unit(self):
+        """
+        """
+        return self._unit
