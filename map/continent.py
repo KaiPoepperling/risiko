c2c6a5b4e6a2767edda9f3999ba2c8677eab5e1b
Kai Pöpperling <kai.poepperling@5minds.de>
2016-06-02 10:59:48 +0200

    Add region and continent implementation, last standing

--- map/continent.py
+++ map/continent.py
@@ -1,3 +1,42 @@
 class Continent(object):
     """
     """
+    def __init__(self):
+        """
+        """
+        self._owner = None
+        self._regions = dict()
+
+    @property
+    def owner(self):
+        """
+        """
+        return self._owner
+
+    @property
+    def regions(self):
+        """
+        """
+        return self._regions
+
+    def add_region(self, region):
+        """
+        """
+        self._regions[region.name] = region
+
+    def check_continent_has_owner(self):
+        """
+        """
+        owner = None
+        for region in self._regions:
+            if region.owner is not None:
+                if owner is None:
+                    owner = region.owner
+                if owner is not region.owner:
+                    # Log continent has not just one owner
+                    return false
+                else:
+                    self._owner = owner
+            else:
+                # Log region has no owner
+
