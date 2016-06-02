2b59291b851552ceeab2221f7b6e3f0c5e58014f
Kai Pöpperling <kai.poepperling@5minds.de>
2016-06-02 10:58:06 +0200

    Add goal card implementation

--- cards/goal/goal_card.py
+++ cards/goal/goal_card.py
@@ -1,7 +1,27 @@
 class GoalCard(object):
     """
     """
+    def __init__(self, number, description, army):
+        """
+        """
+        self._number = number
+        self._description = description
+        self._army = army
 
-    def __init__(self):
+    @property
+    def number(self):
         """
         """
+        return self._number
+
+    @property
+    def description(self):
+        """
+        """
+        return self._description
+
+    @property
+    def army(self):
+        """
+        """
+        return self._army
