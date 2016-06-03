3a62dbf4e0093d3007f943548d5fdb60e7780173
Kai Pöpperling <kai.poepperling@5minds.de>
2016-06-03 10:09:48 +0200

    Remove previous code from the root folder

--- player/player.py
+++
@@ -1,9 +0,0 @@
-class Player(object):
-    """
-    """
-    def __init__(self, name, color):
-        """
-        """
-        self._name = name
-        self._color = color
-        self._hand = None
